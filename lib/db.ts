bc666ea367527511e3493196a4e19dc20dde5d32
zakiezabar <zakiezabar@gmail.com>
2025-03-21 19:39:35 +0800

    update dbname

--- lib/db.ts
+++ lib/db.ts
@@ -21,7 +21,7 @@ export const connect = async () => {
     
     cached.promise = cached.promise ||
     mongoose.connect(MONGODB_URL, {
-        dbName: 'event',
+        dbName: 'clerk-db',
         bufferCommands: false,
         connectTimeoutMS: 30000,
     })
